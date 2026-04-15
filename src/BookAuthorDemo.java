257180da0d00f07da6d20396e59de93acfb6c4b3
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 09:58:51 +0400

    add task 16- BookAuthorDemo

--- src/BookAuthorDemo.java
+++ src/BookAuthorDemo.java
@@ -14,6 +14,26 @@ public class BookAuthorDemo {
 
         System.out.println(  bookAuthorRecords); //  Display all elements in the HashMap
 
+        //display the author of one specific book
+        System.out.println("the author of Moby Dick is :" +bookAuthorRecords.get("Moby Dick") );
+
+        // check if key (To Kill a Mockingbird) exist in HashMap or not
+         System.out.println("is To Kill a Mockingbird exist in HashMap ?" + bookAuthorRecords.containsKey("To Kill a Mockingbird"));
+
+         //  Update the author of specific book
+         bookAuthorRecords.put("Moby Dick", "Herman Melville (Updated)");
+
+         // Display the updated HashMap
+        System.out.println( "\n updated HashMap: " +"\n"+ bookAuthorRecords);
+
+        //delete one book record
+        bookAuthorRecords.remove("1984");
+
+        // Display the updated HashMap after removal
+        System.out.println( "\n updated HashMap after removal:" +"\n"+bookAuthorRecords);
+
+        // display the total number of records
+        System.out.println("\nTotal number of books: " + bookAuthorRecords.size());
 
     }
 }
