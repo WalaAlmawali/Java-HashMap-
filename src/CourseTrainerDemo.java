df1a4130e16c3cd4dd13d7c18dc6d5afd0fff64a
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 10:04:54 +0400

    Declare a HashMap variable + Add course-trainer records using the put() method+  Display all elements in the HashMap

--- src/CourseTrainerDemo.java
+++ src/CourseTrainerDemo.java
@@ -3,6 +3,19 @@ import java.util.*;
 public class CourseTrainerDemo {
     public static void main (String[] args){
 
+        HashMap<String, String>   courseTrainerRecords = new HashMap<>();
+
+        //Add  course-trainer records  in HashMap
+        courseTrainerRecords.put("Java Programming", "Mr. Ahmed");
+        courseTrainerRecords.put("Web Development", "Ms. Sara");
+        courseTrainerRecords.put("Data Structures", "Dr. Ali");
+        courseTrainerRecords.put("Database Systems", "Mr. Hassan");
+        courseTrainerRecords.put("Cyber Security", "Ms. Fatima");
+
+        //  Display all elements in the HashMap
+        System.out.println(  courseTrainerRecords);
+
+        
 
 
 
