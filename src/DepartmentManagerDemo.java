01fc1b3754c47d861250c6e62688721e656097fa
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 11:06:04 +0400

    task 25 part 8 and 9 - add department-manager records using the put() method

--- src/DepartmentManagerDemo.java
+++ src/DepartmentManagerDemo.java
@@ -4,5 +4,13 @@ public class DepartmentManagerDemo {
 
         HashMap<String, String>  departmentManagerRecords = new HashMap<>();
 
+        // Add  department-manager records
+
+        departmentManagerRecords.put("Human Resources", "Aisha Al-Maamari");
+        departmentManagerRecords.put("Finance", "Omar Al-Balushi");
+        departmentManagerRecords.put("IT Department", "Sara Al-Harthy");
+        departmentManagerRecords.put("Marketing", "Khalid Al-Riyami");
+        departmentManagerRecords.put("Operations", "Fatima Al-Nabhani");
+
     }
 }
