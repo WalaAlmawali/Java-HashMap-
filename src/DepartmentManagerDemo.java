ce1256747835df170566f7a641f5f99aef147980
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-15 11:03:35 +0400

    task 25 part 4 to 7

--- src/DepartmentManagerDemo.java
+++ src/DepartmentManagerDemo.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class DepartmentManagerDemo {
     public static void main (String[] args){
 
+        HashMap<String, String>  departmentManagerRecords = new HashMap<>();
 
     }
 }
